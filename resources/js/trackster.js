a027289c37f37c3b3dfe6ddc85826e5cdd7699cf
Chip-L <chip_long@yahoo.com>
2017-12-26 20:09:50 -0700

    jquery works now and populates the page correctly. time to add the sort

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -32,7 +32,59 @@ Trackster.goSearch = function() {
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
 */
-Trackster.renderTracks = function(tracks) {
+Trackster.renderTracks = function (tracks) {
+  console.log('renderTracks');
+  var $trackBody = $('#track-list tbody');
+
+  $trackBody.empty();
+
+  for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    var track = tracks[trackIndex];
+    var trackImage = "";
+
+    if(track.image[3]['#text'] !== "") {
+      trackImage = track.image[3]['#text'];
+    } else if(track.image[2]['#text'] !== "") {
+      trackImage = track.image[2]['#text'];
+    } else if(track.image[1]['#text'] !== "") {
+      trackImage = track.image[1]['#text'];
+    } else { //  if(track.image[0]['#text'] <> "") and if it does, assign ""
+      trackImage = track.image[0]['#text'];
+    };
+
+    $trackBody.append('<tr>');
+    var $row = $('#track-list tbody tr:last');
+
+    $row.append('<td></td>');
+    $row.append('<td><a href="'+ track.url + '" target="_blank" rel="noreferrer noopener">' +
+    '      <i class="fa fa-play-circle-o fa-3x play-button" aria-label="play"></i>' +
+    '    </a></td>');
+    $row.append('<td>' + track.name + '</td>');
+    $row.append('<td>' + track.artist + '</td>');
+
+    if(trackImage !== "") {
+      $row.append('<td>' +
+      '    <div class="img-container"> ' +
+      '      <img src="' + track.image[3]['#text'] + '" alt="Alblum cover">' +
+      '    </div>' +
+      '  </td>');
+    } else {
+      $row.append('<td>' +
+      '    <div class="img-container"> ' +
+      '      <i class="fa fa-picture-o" aria-hidden="true"></i>' +
+      '    </div>' +
+      '  </td>');
+    }
+
+    $row.append('<td>' + track.listeners + '<td>');
+  } // end for
+}
+
+/*
+  Given an array of track data, create the HTML for a Bootstrap row for each.
+  Append each "row" to the container in the body to display all tracks.
+*/
+Trackster.renderTracksNotTable = function(tracks) {
   var $trackList = $('#track-list');
   var trackURL = "",
     song = "",
@@ -77,6 +129,7 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
+  console.log('url: http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',);
   $.ajax({
     url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
     datatype: 'jsonp',
